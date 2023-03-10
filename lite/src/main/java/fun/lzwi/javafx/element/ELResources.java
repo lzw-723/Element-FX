ec87d9324cd56f77f3353260d09d2079ee8d3cb2
lzw-723 <lzw-723@qq.com>
2023-03-10 20:48:58 +0800

    完善Demo

--- lite/src/main/java/fun/lzwi/javafx/element/ELResources.java
+++ lite/src/main/java/fun/lzwi/javafx/element/ELResources.java
@@ -1,10 +1,18 @@
 package fun.lzwi.javafx.element;
 
-import java.net.URI;
 import java.net.URL;
 
 public class ELResources {
+    
+    public static String version() {
+        return "0.0.1-dev";
+    }
+
     public static URL load(String path) {
         return ELResources.class.getResource(path);
+    }
+
+    public static URL loadStyle() {
+        return load("css/element.css");
     }
 }
